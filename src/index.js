c1913c45cd1928f7dc0bdeebb23fca4b89f214c9
Tomasz Skolimo <skolim10@gmail.com>
2022-10-13 13:55:53 +0200

    hw1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import user from './components/social-profile/user.json';
+
+import { Profile } from './components/social-profile/Profile';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </App>
   </React.StrictMode>
 );
