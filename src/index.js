b618d596075f5be0bcbff865901d2dfc15ebcd38
Tomasz Skolimo <skolim10@gmail.com>
2022-11-09 02:11:06 +0100

    del

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { App } from 'components/App';
 import './index.css';
 
 import user from './components/social-profile/user.json';
+<<<<<<< HEAD
 import data from './components/statistics/data.json';
-import friends from './components/friend-list/friends.json';
-import transactions from './components/transaction-history/transactions.json';
 
 import { Profile } from './components/social-profile/Profile';
 import { Statistics } from './components/statistics/Statistics';
+<<<<<<< HEAD
 import { FriendList } from './components/friend-list/FriendList';
 import { TransactionHistory } from './components/transaction-history/TransactionHistory';
+=======
+
+import { Profile } from './components/social-profile/Profile';
+>>>>>>> parent of 9272c35 (update)
+=======
+>>>>>>> parent of 8a0fea7 (fourCase)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -23,9 +29,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
+<<<<<<< HEAD
+<<<<<<< HEAD
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
+=======
+>>>>>>> parent of 9272c35 (update)
+=======
+      <Statistics title="Upload stats" stats={data} />;
+      <Statistics stats={data} />;
+>>>>>>> parent of 8a0fea7 (fourCase)
     </App>
   </React.StrictMode>
 );
